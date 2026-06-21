29b50217a64e0c7010bacdc98b6f28d9a3b22ecd
Siddhartha Jha <114568977+cerebralassassin30@users.noreply.github.com>
2026-06-21 16:50:31 +0530

    hs_bhmdl.v

---
+++ hs_bhmdl.v
@@ -0,0 +1,43 @@
+// VERILOG CODE
+
+`timescale 1ns/1ps
+
+module hs_bhmdl(d,br,a,b);
+input a,b;
+output reg d,br;
+always@(*) begin
+d = a ^ b;
+br = (~a) & b;
+end
+endmodule
+
+// TESTBENCH 
+
+`timescale 1ns/1ps
+
+module hs_bhmdl_tb;
+reg A,B;
+wire D,BR;
+initial begin
+$monitor("simtime = %0t, D= %b, BR = %b, A = %b, B= %b", $time, D,BR,A,B);
+end
+hs_bhmdl dut(.d(D), .br(BR), .a(A), .b(B));
+  
+initial begin
+  
+// TESTCASE 1
+    A = 1'b0; B = 1'b0;
+  
+// TESTCASE 2
+#10 A = 1'b0; B = 1'b1;
+  
+// TESTCASE 3
+#10 A = 1'b1; B = 1'b0;
+  
+// TESTCASE 4
+#10 A = 1'b1; B = 1'b1;
+
+#10 $finish;
+
+end
+endmodule
