398d98b6211241ac76f0c1ffbfb727d57c7d2584
Siddhartha Jha <sidranchi75@gmail.com>
2026-06-22 08:40:10 +0530

    mux_2x1_if_else_tb.v

---
+++ 2X1 MUX/2X1 MUX using if else statement/mux_2x1_if_else_tb.v
@@ -0,0 +1,26 @@
+// TESTBENCH
+
+`timescale 1ns/1ps
+
+module mux_2x1_if_else_tb;
+  reg [1:0]I;
+  reg S;
+  wire Y;
+initial begin
+$monitor("simtime = %0t, Y = %b, I = %b, S = %b", $time, Y,I,S);
+end
+mux_2x1_if_else dut(.y(Y), .i(I), .s(S));
+initial begin
+     //TESTCASE 1
+      I[1] = 1'b0; I[0] = 1'b1; S= 1'b0; // Expected Output, Y = I[0] = 1'b1
+    // TESTCASE 2
+  #10 I[1] = 1'b0; I[0] = 1'b1; S= 1'b1; // Expected Output, Y = I[1] = 1'b0
+    // TESTCASE 3
+  #10 I[1] = 1'b1; I[0] = 1'b0; S= 1'b0; // Expected Output, Y = I[0] = 1'b0
+    // TESTCASE 4
+  #10 I[1] = 1'b1; I[0] = 1'b0; S= 1'b1; // Expected Output, Y = I[1] = 1'b1
+  
+  #10 $finish;
+end
+endmodule
+  
