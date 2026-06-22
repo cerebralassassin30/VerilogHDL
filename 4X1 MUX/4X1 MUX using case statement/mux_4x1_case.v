eb674311c627700c7bd565969f7822f414b39806
Siddhartha Jha <sidranchi75@gmail.com>
2026-06-22 09:59:44 +0530

    Create mux_4x1_case.v

---
+++ 4X1 MUX/4X1 MUX using case statement/mux_4x1_case.v
@@ -0,0 +1,18 @@
+//VERILOG CODE
+
+`timescale 1ns/1ps
+
+module mux_4x1_case(y,s,i);
+  input [3:0]i;
+  input [1:0]s;
+output reg y;
+always @(*) begin 
+case(s)
+  2'b00: y = i[0];
+  2'b01: y = i[1];
+  2'b10: y = i[2];
+  2'b11: y = i[3];
+  default: y = 1'b0;
+endcase
+end
+endmodule
